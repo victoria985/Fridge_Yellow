1cb8adece56c8cb0db6737043e5d092809b441f7
Evaldas Gegeckas <59705811+TheDarkBlueSnaper@users.noreply.github.com>
2024-01-04 07:56:31 +0200

    1.18

--- saldytuvas.py
+++ saldytuvas.py
@@ -1,23 +1,3 @@
-""" Komandinio darbo užduotis
-===[ Šaldytuvas ]===
-
-Reikalavimai:
-
-* User inputo, terminalo meniu, iseiga.
-* Šaldytuvo turinys - žodynas, kurio raktas yra produkto pavadinimas, reikšmė - kiekis (float). Yra
-* Pridėti produktą į šaldytuvą. Pridedant egzistuojantį produktą, kiekiai sudedami su esančiais. Yra
-* Išimti produktą iš šaldytuvo. Išimant egzistuojantį produktą, kiekis atitinkamai sumažinamas. Yra
-* Patikrinti, ar reikiamas produkto kiekis yra šaldytuve. Yra
-* Išspausdinti visą šaldytuvo turinį su kiekiais. Reikia
-
-BONUS:
-
-* Patikrinti, ar receptas išeina. 
-** Recepto įvedimas vyksta viena eilute, kuri po to išdalinama. Pva.: Sūris: 0.5, Pomidoras: 2, Duona: 0.4
-*** Jeigu receptas neišeina, išvardinti kiek ir kokių produktų trūksta.
-
-"""
-
 import time
 
 def add_product(fridge_content, product, quantity = 0):
