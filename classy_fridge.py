14cb9438195b83b70391c4793706b82c95912dd9
Evaldas Gegeckas <59705811+TheDarkBlueSnaper@users.noreply.github.com>
2024-01-09 14:58:19 +0200

    Alpha 0.2

--- classy_fridge.py
+++ classy_fridge.py
@@ -1,6 +1,38 @@
 import os
 
+
+class Product:
+    def __init__(self, name:str, quantity:float, **kwargs) -> None:
+        self.name = name
+        self.quantity = quantity
+        self.unit_of_measurement = 'unit' # options: kg, g, L, ml
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __str__(self) -> str:
+        return f"{self.name}: {self.quantity}"
+    
+    def __repr__(self) -> str:
+        return f"({self.name}, {self.quantity})"
+
+
+class Recipe:
+    ingredients = []
+    instructions = []
+
+    def add_ingredient(self, product:Product):
+        self.ingredients.append(product)
+
+    def change_ingredient_quantity(self, ingredient_id:int, new_quantity:float):
+        self.ingredients[ingredient_id].quantity = new_quantity
+
+    def remove_ingredient(self, ingredient_id:int):
+        self.ingredients.pop(ingredient_id)
+
+
 class Smart_Fridge:
+
+    # Defining class
     def __init__(self, user_name, pin_code, temperature=5):
         self.user_name = user_name
         self.__pin = pin_code
@@ -9,6 +41,34 @@ class Smart_Fridge:
     # String representation of a class for checks
     def __str__(self):
         return f'{self.user_name}:{self.__pin}:{self.__temperature}'
+    
+    # Product check function
+    def check_product(self, product_name:str) -> (int, Product):
+        for product_id, product in enumerate(self.contents):
+            if product.name == product_name:
+                return product_id, product
+        return None, None
+    
+    # Product quantity check function
+    def check_product_quantity(self, product:Product, quantity:float):
+        return product.quantity - quantity
+
+    # Add product function
+    def add_product(self, name:str, quantity:float):
+        product_id, product = self.check_product(name) # nenaudojamus kintamuosius galima vadinti tiesiog _
+        if product is not None:
+            product.quantity += quantity
+        else:
+            self.contents.append(Product(name, quantity))
+    
+    def remove_product(self, name:str, quantity:float):
+        pass
+
+    def print_contents(self):
+        pass
+
+    def check_recipe(self, recipe:Recipe):
+        pass
 
     # User input function to get user name and pin code
     @staticmethod
@@ -55,13 +115,15 @@ class Smart_Fridge:
                 break
             else:
                 print("PIN code does not match. Please try again.")
-
+    
+    # Main function of runing the fridge
     def main(self):
         if os.path.isfile('user_data.txt'):
             print("User data file already exists.")
             user_name, pin_code = self.read_user_data_from_file()
             self.user_name = user_name
             self.__pin = pin_code
+            print(f"Welcome back, {user_name}")
             self.confirm_pin()
         else:
             print("User data file does not exist.")
